46f3d8343576a7aa71c38769e13d09dd400f61a2
Tom Brow <tom@tombrow.com>
2021-01-16 21:56:54 -0600

    key map

--- keyboards/balbuzard_pro/keymaps/default/keymap.c
+++ keyboards/balbuzard_pro/keymaps/default/keymap.c
@@ -15,45 +15,36 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-};
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,       KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-              KC_1, KC_2, KC_3, KC_4,       KC_5, KC_6, KC_7, KC_8
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RCTL_T(KC_SCLN),
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+        KC_NO, MO(2), KC_LSFT, KC_LGUI, RALT_T(KC_ENT), KC_SPC, MO(1), KC_NO
+    ),
+
+    [1] = LAYOUT(
+        KC_GRV, KC_7, KC_8, KC_9, KC_MINS, KC_NO, KC_UNDS, KC_EXLM, KC_DQUO, KC_PLUS,
+        KC_LALT, KC_4, KC_5, KC_6, KC_DOT, KC_CIRC, KC_MINS, KC_EQL, KC_DLR, KC_RCTL,
+        KC_0, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_RABK, KC_BSLS,
+        KC_NO, MO(3), KC_LSFT, KC_LGUI, KC_NO, KC_NO, KC_TRNS, KC_NO
+    ),
+
+    [2] = LAYOUT(
+        KC_NO, KC_AT, KC_LPRN, KC_RPRN, KC_NO, KC_AMPR, KC_LBRC, KC_RBRC, KC_QUOT, KC_PERC,
+        KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ESC,
+        KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_TILD, KC_ASTR, KC_HASH, KC_NO, KC_SLSH,
+        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_ENT, KC_BSPC, KC_TAB, KC_NO
+    ),
+
+    [3] = LAYOUT(
+        KC_NO, KC_NO, LCTL(KC_SLCK), LCTL(KC_PAUS), RESET, KC_NO, KC_SLCK, KC_PAUS, KC_NO, KC_NO,
+        KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO
     ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
     return true;
 }
